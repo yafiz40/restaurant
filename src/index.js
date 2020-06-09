59038f36f314a2c6241450d0c387c0f51bf1b413
RaniaNorAida <48505011+RaniaNorAida@users.noreply.github.com>
2020-06-09 19:31:47 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './styles/style.css';
+import './js/tables.js';
+import './js/orders.js';
+
 // import x from "./js/x.png";
 // import './x.html';
 
@@ -12,3 +15,14 @@ const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 // Add the image to our existing div.
 // const myIcon = new Image();
 // myIcon.src = images['mix.jpg']['default'];
+
+import table from './js/tables.js';
+var path = window.location.pathname;
+var page = path.split("/").pop();
+if (page == 'menu.html') {
+	if (table != -1) {
+		// console.log("BERHASIL MASUK HALAMA PAGE");
+	}else{
+		// window.location.href = "index.html";
+	}
+}
